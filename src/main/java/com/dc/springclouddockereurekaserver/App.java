6c3f5685a7abe3a0d23921e06cb14299bd4c2168
ChunMianBuJueXiao123 <929878802@qq.com>
2017-10-27 11:30:52 +0800

    the first commit

---
+++ src/main/java/com/dc/springclouddockereurekaserver/App.java
@@ -0,0 +1,19 @@
+package com.dc.springclouddockereurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 使用docker打包部署eureka-server的运行环境
+ *
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
